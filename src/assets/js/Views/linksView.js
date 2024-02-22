9f4399a9c0dbf9560762ee70ccefc38232d3fb56
YariMorcus <yarimorcus@zeelandnet.nl>
2024-02-22 14:51:17 +0100

    indicate current page

---
+++ src/assets/js/Views/linksView.js
@@ -0,0 +1,49 @@
+import View from './View';
+
+/**
+ * @class linksView
+ * @extends View the parent view (dashboard)
+ * @property {Object} parentEl - the report for navigation
+ */
+class linksView extends View {
+  #parentEl = document.querySelector('.report-for__nav');
+  #buttons = this.#parentEl.querySelectorAll('.report-for__nav-button');
+
+  /**
+   * Listen for submit event on linksView parent element
+   * @param {Object} handler the handler that will be called when click happens on a nav-button
+   */
+  addHandlerClick(handler) {
+    this.#parentEl.addEventListener('click', e => {
+      const button = e.target.classList.contains('report-for__nav-button');
+
+      if (!button) return;
+
+      // Pass current button to handler
+      handler(e.target);
+    });
+  }
+
+  /**
+   * Indicate which page user is on
+   * @param {Object} button the selected button
+   */
+  indicateCurrentPage(button) {
+    this.#buttons.forEach(navButton => {
+      navButton.classList.remove('is-active');
+    });
+
+    // Indicate current page
+    button.classList.add('is-active');
+  }
+
+  /**
+   * Returns current page id
+   * @returns {string} id - id of current page
+   */
+  getPageId(button) {
+    return button.id;
+  }
+}
+
+export default new linksView();
---
+++ src/assets/js/controller.js
@@ -0,0 +1,25 @@
+import * as model from './model';
+import linksView from './Views/linksView';
+
+/**
+ * Controller provides functionality for the navigation
+ */
+const navController = async function navController(button) {
+  // Indicate and save current page
+  linksView.indicateCurrentPage(button);
+  model.state.curPage = linksView.getPageId(button);
+
+  // 2. Retrieve and store data from json file
+  // 3. Render given data for work
+  // 4. Render given data for play
+  // 5. Render given data for study
+  // 6. Render given data for exercise
+  // 7. Render given data for social
+  // 8. Render given data for self care
+};
+
+const init = function init() {
+  linksView.addHandlerClick(navController);
+};
+
+init();
